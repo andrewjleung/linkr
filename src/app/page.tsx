314184e0260ecacb1f4c97aab8d163f561547e0c
Andrew Leung <andrewleung104@gmail.com>
2023-06-24 00:35:52 -0500

    Add dialog link form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import prisma from "@/lib/prisma";
-import { CreateLinkForm } from "@/components/CreateLinkForm";
+import { CreateLinkForm } from "@/components/create-link-form";
 
 export default async function Home() {
   const foo = await prisma.link.findMany();
